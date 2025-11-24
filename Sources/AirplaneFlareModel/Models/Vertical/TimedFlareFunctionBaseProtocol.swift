2ac9efed220e26acb603d6597b928ed15dfe5c29
RapboyGao <atgao74201@qq.com>
2025-11-24 11:02:55 +0800

    docs(Vertical/TimedFlareFunctionBaseProtocol): 添加minX1和maxX1的计算方法并优化注释格式
    为TimedFlareFunctionBaseProtocol协议添加了minX1和maxX1的计算方法，并通过扩展实现默认计算逻辑
    同时调整了注释格式，使其更清晰易读

--- Sources/AirplaneFlareModel/Models/Vertical/TimedFlareFunctionBaseProtocol.swift
+++ Sources/AirplaneFlareModel/Models/Vertical/TimedFlareFunctionBaseProtocol.swift
@@ -1,31 +1,44 @@
 import Foundation
 
 public protocol TimedFlareFunctionBaseProtocol: Codable, Sendable, Hashable {
-    // MARK: - Given Conditions
+  /// y(0)
+  /// 可带入初始升降率(例如-800)
+  var y0: Double { get set }
 
-    /// y(0)
-    /// 可带入初始升降率(例如-800)
-    var y0: Double { get set }
+  /// y(x1)
+  /// 可带入结束升降率(例如-150)
+  var y1: Double { get set }
 
-    /// y(x1)
-    /// 可带入结束升降率(例如-150)
-    var y1: Double { get set }
+  /// ∫0→x1 y(x) dx
+  /// 可带入总消失高度(-50)
+  var h1: Double { get set }
 
-    /// ∫0→x1 y(x) dx
-    /// 可带入总消失高度(-50)
-    var h1: Double { get set }
+  /// upper bound x1 (> 0)
+  /// 可带入总时间(分钟数)
+  var x1: Double { get set }
 
-    /// upper bound x1 (> 0)
-    /// 可带入总时间(分钟数)
-    var x1: Double { get set }
+  var minX1: Double { get }
 
-    init(y0: Double, y1: Double, x1: Double, h1: Double)
+  /// x1可能的最大值
+  var maxX1: Double { get }
 
-    /// ∫0→x y(x) dx
-    /// 可带入时间(分钟数)
-    func integral(atX x: Double) -> Double
+  init(y0: Double, y1: Double, x1: Double, h1: Double)
 
-    /// y(x)
-    /// 可带入时间(分钟数)
-    func y(atX x: Double) -> Double
+  /// ∫0→x y(x) dx
+  /// 可带入时间(分钟数)
+  func integral(atX x: Double) -> Double
+
+  /// y(x)
+  /// 可带入时间(分钟数)
+  func y(atX x: Double) -> Double
+}
+
+extension TimedFlareFunctionBaseProtocol {
+  public var minX1: Double {
+    2 * h1 / (y0 + y1)
+  }
+
+  public var maxX1: Double {
+    2 * h1 / y1
+  }
 }
